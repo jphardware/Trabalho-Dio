deb078404eb71bf4a32d267d3c096711dad9342a
jphardware <jphardware2015.rj@gmail.com>
2024-01-15 21:55:54 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let nome = "Heitor"
+let nivel = 10001
+let ferro ="Ferro"
+let bronze = "Broze"
+let prata = "Prata"
+let ouro = "Ouro"
+let platina = "Platina"
+let ascendente = "Ascendente"
+let imortal = "Imortal"
+let radiante = "Radiante"
+
+if(nivel <= 1000){
+    console.log("O Herói de "+ nome + " está no nivel " + ferro)}
+    
+        else if(nivel <=2000){
+        console.log("O Herói de mome "+nome+ "está no nivel " + bronze)}
+    
+        else if(nivel <=5000){
+        console.log("O Herói de mome "+nome+ "está no nivel " + prata)}
+        
+        else if(nivel <=7000){
+        console.log("O Herói de mome "+nome+ "está no nivel " +ouro)}
+        
+        else if(nivel <=8000){
+        console.log("O Herói de mome "+nome+ "está no nivel " +platina)}
+
+        else if(nivel <=9000){
+        console.log("O Herói de mome "+nome+ "está no nivel " +ascendente)}
+        
+        else if(nivel <=10000){
+        console.log("O Herói de mome "+nome+ "está no nivel " + imortal)}
+
+        else if(nivel >=10001){
+        console.log("O Herói de mome "+nome+ "está no nivel " +radiante)}
+
+
+    
